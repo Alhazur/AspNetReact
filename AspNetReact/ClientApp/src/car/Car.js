4032651022b1a4a64f6079a63dcb1496c5939fc6
deltagare <deltagare@LEXJONKAF32>
2019-05-03 16:11:59 +0200

    ret

--- AspNetReact/ClientApp/src/car/Car.js
+++ AspNetReact/ClientApp/src/car/Car.js
@@ -1,1 +1,25 @@
-﻿
+﻿import React, { Component } from 'react';
+
+export default function(){
+    return [
+        {
+            id: 1,
+            model: "M5",
+            brand: "Bmw",
+            year: 2019
+        },
+        {
+            id: 2,
+            model: "C63 AMG",
+            brand: "Medcedes-Benz",
+            year: 2019
+        },
+        {
+            id: ,
+            model: "R8",
+            brand: "Audi",
+            year: 2019
+        }
+    ]
+}
+
